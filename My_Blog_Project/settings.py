a608f08bddad61686d240c789721e656b0062b81
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-07 23:56:42 +0600

    4.signup done

--- My_Blog_Project/settings.py
+++ My_Blog_Project/settings.py
@@ -40,8 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App_Blog',
-    'App_Login'
+    'App_Login',
+    'crispy_forms',
+
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
